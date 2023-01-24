2d8ab55cd61ce0ac3ed246d4458f3693e532a189
RomanOrtiz75 <romanabrahanortiz@gmail.com>
2023-01-23 19:10:24 -0600

    lab
    escuela

--- src/components/PrimerComponente.js
+++ src/components/PrimerComponente.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand
+} from 'mdb-react-ui-kit';
 
 export const PrimerComponente = () => {
   return (
-    <div>PrimerComponente</div>
-  )
+    <>
+      <MDBNavbar light bgColor='light'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>FENAMAC</MDBNavbarBrand>
+        </MDBContainer>
+      </MDBNavbar>
+
+      <br />
+
+      <MDBNavbar light bgColor='dark'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand tag="span" className='mb-0 h1'>Navbar</MDBNavbarBrand>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
+  );
 }
