a9f1b9076bceb632f946381afdbef2615727110d
RomanOrtiz75 <romanabrahanortiz@gmail.com>
2023-02-16 18:28:46 -0600

    nuevo

--- src/components/SegundoComponente.js
+++ src/components/SegundoComponente.js
@@ -12,48 +12,75 @@ export const SegundoComponente = () => {
       }} />
 
 {/* searchbar */}
-      <div style={{
-              width: '100%',
-              position: 'absolute',
-              display: 'flex'
-            }}>
-                <form action="/" method="get" style={{
-                  position: 'absolute'
-                }
-                }>
-                    <label htmlFor="header-search">
-                        <span className="visually-hidden">BUSCADOR DE RATING</span>
-                    </label>
-                <input
-                    type="text"
-                    id="header-search"
-                    placeholder="BUSCAR POR NOMBRE,APELLIDO,NO. DE RATING ETC."
-                    name="s" 
-                    style={{
-                      width: '100%',
-                      border: '3px solid #00B4CC',
-                      borderright: 'none',
-                      padding: '5px',
-                      height: '20px',
-                      outline: 'none',
-                      color: '#9DBFAF',
-                    }}
-                />
-                <button type="submit" style={{
-                  width: '40px',
-                  height: '36px',
-                  border: '1px solid #00B4CC',
-                  background: '#00B4CC',
-                  textalign: 'center',
-                  color: '#fff',
-                  borderradius: '0 5px 5px 0',
-                  cursor: 'pointer',
-                  fontsize: '20px',
-                }}>BUSCAR
-                </button>
-                </form>
-            </div>
-      </div>
+
+          <div>
+
+              <form action="/" method="get" style={{
+                position: 'absolute',
+
+            right: '34%',
+
+            bottom: '20%'
+
+          
+
+          }
+
+          }>
+
+          <input
+
+              type="text"
+
+              id="header-search"
+
+              placeholder="BUSCAR POR NOMBRE,APELLIDO,NO. DE RATING ETC."
+
+              name="s"
+
+              style={{
+
+                width: '500px',
+
+                borderright: 'none',
+
+                padding: '5px',
+
+                height: '40px',
+
+                outline: 'none',
+
+              }}
+
+          />
+
+          <button type="submit" style={{
+
+            width: '100px',
+
+            height: '40px',
+
+            background: 'rgba(172, 54, 61,1)',
+
+            textalign: 'center',
+
+            color: '#fff',
+
+            border: 'solid 1px rgba(172, 54, 61,1)',
+
+            cursor: 'pointer',
+
+            fontsize: '20px',
+
+          }}>BUSCAR
+
+          </button>
+
+          </form>
+
+          </div>
+
+          </div>
 
   )
 }
