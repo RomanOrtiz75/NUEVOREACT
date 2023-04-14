eb5ea68d142b461b18b3983ab32609316a81a87d
RomanOrtiz75 <romanabrahanortiz@gmail.com>
2023-04-14 00:44:05 -0600

    holi cx
    xd

--- src/server.js
+++ src/server.js
@@ -1,18 +1,12 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const app = express()
-const port = 5000
+const URI = 'mongodb+srv://roman:residentrm1@cluster0.aivqn0o.mongodb.net/test'
 
-mongoose.connect('mongodb://localhost:27017/FENAMAC', {
+mongoose.connect( URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
+});
 const connection = mongoose.connection
 connection.once('open', () => {
-  console.log("MongoDB database connection established successfully")
-})
-
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`)
-})
+  console.log("Conectado correctamente con la base de datos")
+});
